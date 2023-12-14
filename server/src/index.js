9a02ba19c4f5e1b0d69a7dfab6b17340e8ebc5e7
Faegheh <59620602+faeghehmohammadian@users.noreply.github.com>
2023-12-14 22:07:00 +0400

    server side done

---
+++ server/src/index.js
@@ -0,0 +1,22 @@
+const express =require('express');
+const app =express();
+const dotenv=require('dotenv');
+const mongoose = require('mongoose');
+const router=require('./router')
+const morgan=require('morgan')
+const cors=require('cors')
+
+dotenv.config();
+app.use(express.json())
+app.use(cors());
+app.use(morgan('tiny'));
+app.use(router);
+app.get("/",(req,res)=>{
+    res.send('Hello');
+});
+
+mongoose.connect(process.env.MONGO_URL).then(()=>{
+    app.listen(8080, () => {
+        console.log(`Mongoose Connect and Server running on PORT: 8080/`);
+    });
+})
---
+++ server/src/router.js
@@ -0,0 +1,31 @@
+const express = require('express');
+const router = express.Router();
+
+const jwt=require('jsonwebtoken');
+const isLoggedIn = require('./middleware/isLoggedIn');
+
+const updateTodoRoute= require('./routers/updateTodoRoute')
+const createTodoRoute= require('./routers/createTodoRoute')
+const readTodoRoute= require('./routers/readTodoRoute')
+const deleteTodoRoute= require('./routers/deleteTodoRoute')
+
+router.post("/login",(req,res)=>{
+    if (req.body.password === process.env.PASSWORD){
+        if (req.body.password === process.env.PASSWORD){
+            const token = jwt.sign({
+                userId:1,
+            },process.env.SECRET);
+            res.json({
+                token,
+            });
+    }
+}else{
+    res.status(401).send('Warng Password');
+} 
+});
+router.get('/todos',isLoggedIn, readTodoRoute)
+router.post('/todos',isLoggedIn, createTodoRoute)
+router.put('/todos/:id',isLoggedIn, updateTodoRoute)
+router.delete('/todos/:id',isLoggedIn, deleteTodoRoute)
+
+module.exports=router;
